a0ae075471e75513e1a690f56db912e8bd29f024
Tanishk_Senpai <tanishksingh832@gmail.com>
2021-10-20 08:38:57 +0530

    Create Basic Calculator.py

---
+++ Basic Calculator.py
@@ -0,0 +1,6 @@
+print("Enter Your 1st Number")
+inpnum1 = input()
+print("Now, Enter Your 2nd Number")
+inpnum2 = input()
+
+print("The addition of these two numbers is - " , int(inpnum1)+int(inpnum2) )
